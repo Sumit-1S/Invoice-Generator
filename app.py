797ebc810d9cc225e2d4432abac4847af40efe14
Sumit Singh <sumitsingh130801@gmail.com>
2022-05-26 14:13:46 +0530

    Initial

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from urllib import response
 import requests 
 import json
 import io
+from datetime import datetime
 URL = "http://127.0.0.1:8000/data/"
 
 req = requests.get(url = URL)
@@ -16,9 +17,16 @@ expired_item = []
 item_shortage = []
 item_unavailable = []
 
+
+
 for item in json_data:
-  if item.item_quant<10:
-    item_shortage.append(item.item_code)
-  elif item.item_quant==0:
-    item_unavailable.append(item.item_code)
+  if item.get('expiry_date')[:10]<str(datetime.today())[:10]:
+    expired_item.append(item.get('item_code'))
+  elif item.get('item_quant')<100 and item.get('item_quant')>0:
+    item_shortage.append(item.get('item_code'))
+  elif item.get('item_quant')==0:
+    item_unavailable.append(item.get('item_code'))
   
+print(expired_item)
+print(item_shortage)
+print(item_unavailable)
